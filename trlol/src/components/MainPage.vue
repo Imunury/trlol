84c13927073e029698e5f92844f52101d7f554dd
Imunury <dipblue0808@naver.com>
2023-05-07 16:19:34 +0900

    as

--- trlol/src/components/MainPage.vue
+++ trlol/src/components/MainPage.vue
@@ -5,10 +5,10 @@
             <h1>룬</h1>
             <h2>성격 테스트</h2>
         </div>
-        <div><button id="main_start_btn">시작하기</button></div>
+        <div><button id="main_start_btn"><a href="/test">시작하기</a></button></div>
         <div>
             <p>공유하기</p>
-            
+
         </div>
 
     </div>
